1d3cc7d68cfd157352c91667a53d02cb746f1960
o0o-jihyun <79901413+o0o-jihyun@users.noreply.github.com>
2021-03-02 17:35:26 +0900

    Add files via upload

---
+++ Test1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main()
+{	
+	printf("data type 별 byte 수 int : %d byte\n",sizeof(int));
+	printf("data type 별 byte 수 char : %d byte\n",sizeof(char));
+	printf("data type 별 byte 수 long : %d byte\n",sizeof(long));
+	printf("data type 별 byte 수 short int : %d byte\n",sizeof(short int));
+	printf("data type 별 byte 수 float : %d byte\n",sizeof(float));
+	printf("data type 별 byte 수 double : %d byte\n",sizeof(double));
+
+}
+
+
+
+
